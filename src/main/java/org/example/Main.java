0cd16a89dea106b16f0aebcfe25bd439eeb981e2
AKKA\Ivan.FRIAS-GIL <IVAN.frias-gil@akkodis.com>
2023-08-02 16:32:05 +0200

    Introduzco mis datos

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -5,5 +5,6 @@ package org.example;
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world");
+        System.out.println("Mi nombre es Ivan y tengo 26 años.");
     }
 }
