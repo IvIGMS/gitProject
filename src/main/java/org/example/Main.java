8ce4d8a21e871c34d0fb6cfae1c19233c8a09266
Ivan Frias Gil <friasgilivan@gmail.com>
2023-08-02 17:38:13 +0200

    More changes

--- src/main/java/org/example/Main.java
+++ src/main/java/org/example/Main.java
@@ -7,5 +7,6 @@ public class Main {
         System.out.println("Hello world");
         System.out.println("Mi nombre es Ivan y tengo 26 años.");
         System.out.println("Changes");
+        System.out.println("More changes");
     }
 }
